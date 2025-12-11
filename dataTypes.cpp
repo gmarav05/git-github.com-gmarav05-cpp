d756f25188fb246c57036d4e573715a8632efdd3
Aravind <gmarav005@gmail.com>
2025-12-11 23:00:45 +0530

    Add ./dataTypes.cpp

---
+++ dataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    string b = "hello";
+    double c = 89.0;
+    float d = 89.99;
+    char e = 'e';
+    bool f = false;
+    
+    return 0;
+}
+// ./input     g++ input.cpp -o input    
