a8a6c63ee63a33bee4baf80ae8312f8b9c7bfa03
Aravind <gmarav005@gmail.com>
2025-12-11 22:52:28 +0530

    Add Calculator

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << "Total is: " << a + b;
+    return 0;
+}
+// ./input     g++ input.cpp -o input    
