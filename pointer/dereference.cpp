12dbf644bb294723f7623413eaf0f85410d1edca
Aravind <gmarav005@gmail.com>
2025-12-22 23:42:16 +0530

    Add dereference file

--- pointer/dereference.cpp
+++ pointer/dereference.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
     string food = "Rice and Dal";
-    string &meal = food;
+
     string* ptr = &food;
-    cout << &meal << "\n";
+
     cout << food << "\n";
     cout << ptr << "\n";
+    cout << *ptr << "\n";
     return 0;
 }
 
-// * ptr is the pointer variable which store the address of food variable.
+// *ptr is the dereference which store the address of food variable.
